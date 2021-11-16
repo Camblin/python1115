b79441c52e472e3aa44bed59f101417805182935
Hyoenjong <hjl.wtg@gmail.com>
2021-11-16 09:50:12 +0900

    함수 연습 5

--- Function3.py
+++ Function3.py
@@ -1,4 +1,5 @@
 # Function3.py
+# 전역변수 
 x = 1 
 def func(a):
     return a+x
@@ -22,3 +23,37 @@ def times(a=10,b=20):
 print( times())
 print( times(3))
 print( times(3,4))
+
+# 키워드 인자
+def connectURI(server, port):
+    str = "http://" + server + ":" + port
+    return str
+
+print(connectURI("credu.com", "80"))
+print(connectURI(port="8080", server="credu.com"))
+
+
+#가별인자(다양한 상황)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result :
+                result.append(x)
+    return result
+
+# 호출 (중단점, Break Point)
+print( union("HAM", "EGG"))
+print( union("HAM","EGG", "SPAM"))
+
+# 정의되지 않은 인자(필수 입력과 옵션 입력이 있는 경우)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+#호출
+print(userURIBuilder("naver.com", "80", id="kim", passwd="1234"))
+print(userURIBuilder("naver.com", "80", id="kim", passwd="1234",
+    name="mike"))
