369996a04af6c1be0635fb052666473a016fed3b
Hyoenjong <hjl.wtg@gmail.com>
2021-11-16 15:33:48 +0900

    클래스 실습 완료

--- 05/상속01.py
+++ 05/상속01.py
@@ -2,16 +2,24 @@ class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
+    def __del__(self):
+        print("delete Person")
+    def __str__(self):
+        return ("str(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
 class Student(Person):
+    # 상속 받고 덮어쓰기(재정의 overide)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    def __del__(self):
+        print("delete Student")
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
 
 p = Person("전우치", "010-222-1234")
@@ -19,4 +27,5 @@ s = Student("이순신", "010-111-1234", "컴공", "991122")
 print(p.__dict__)
 print(s.__dict__)
 
-
+print(p)
+print(s)
