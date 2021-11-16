6f8705d2ba929831c85d5fbadcbe0b7f8c3c6c8a
Hyoenjong <hjl.wtg@gmail.com>
2021-11-16 09:34:31 +0900

    세번째 연습

---
+++ Function2.py
@@ -0,0 +1,16 @@
+# Function2.py
+
+def setValue(newValue):
+    x = newValue
+
+# 호출
+result = setValue(5)
+print(result)
+
+# Tuple을 리턴
+def times(a,b):
+    return a+b,a*b
+
+# 호출 
+result = times(3,4)
+print(result)
