b6ef9c01a3c62b858b5059956d2ecfae68e359e2
Hyoenjong <hjl.wtg@gmail.com>
2021-11-16 09:37:51 +0900

    세번째 연습

---
+++ Function3.py
@@ -0,0 +1,24 @@
+# Function3.py
+x = 1 
+def func(a):
+    return a+x
+
+# 호출 
+print (func(1))
+
+def func2(a):
+    # 지역 변수 (LGB)
+    x = 2
+    return a+x
+
+#호출
+print(func2(1))
+
+print("기본값 셋팅")
+def times(a=10,b=20):
+    return a*b
+
+# 호출
+print( times())
+print( times(3))
+print( times(3,4))
